da8601c10e7df8cc55343137a0aaa75aba39a51b
lizzybeliy0 <lizibeliy.0@mail.ru>
2026-04-16 10:21:15 +0300

    lab1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import model.Medicine;
+import model.PrescriptionType;
+import model.Sale;
+import repository.DataInitializer;
+import repository.MedicineRepository;
+import repository.Repository;
+import repository.SaleRepository;
+import service.PharmacyService;
+import service.PharmacyServiceInterface;
+import ui.ConsoleUI;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Repository<Medicine, String> medicineRepo = new MedicineRepository();
+        Repository<Sale, String> saleRepo = new SaleRepository();
+
+        DataInitializer.initMedicineRepository(medicineRepo);
+
+        PharmacyServiceInterface service = new PharmacyService(medicineRepo, saleRepo);
+        ConsoleUI ui = new ConsoleUI(service);
+        ui.start();
+    }
+}
+// в репозитории init где тестовые данные - туда вынести - done
+// интерфейс фармаси => его в консоль (у каждого сервиса свой интерфейс) (без этого зависит от конкретики, а не абстракции) - done
